35c5d837ab9fe107be780cd1190885ea1c8562e9
Aditya-Prakash07 <prakashaditya775@gmail.com>
2025-01-14 23:48:02 +0530

    Initial commit

---
+++ src/main/java/com/aditya/main/SpringBootWithDataJpa1Application.java
@@ -0,0 +1,13 @@
+package com.aditya.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithDataJpa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithDataJpa1Application.class, args);
+	}
+
+}
